e4e4cc856eb87c3c03c436e6d7862d945e59bf93
Jose-Henrique-a <henriquecruise@hotmail.com>
2024-04-28 14:50:47 -0300

    fix

--- recursividade2.c
+++ recursividade2.c
@@ -1,1 +1,23 @@
-// teste por enquanto
+#include <stdio.h>
+#include <stdlib.h>
+/*  Aula 153: Funções e procedimentos recursivos 
+    Exemplo: imprimir todos os números de n até 0
+*/
+int imprimir( int x ){
+    if (x == 0)
+        printf(" %d ", x);
+    else{
+        printf(" %d ", x);
+        imprimir(x - 1);
+    }
+}
+
+int main(){
+    int n;
+    printf("Digite um valor maior que zero:  ");
+    scanf("%d", &n);
+    imprimir(n);
+
+    return 0 ;
+}
+
